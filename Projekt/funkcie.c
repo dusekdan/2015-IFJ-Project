0990be2b75cdcc9e78b3045201322898a13c151e
butoramatus <butora.matus@gmail.com>
2014-11-05 18:03:31 +0100

    funkcie
    neskutocne velke zmeny pribudol novy subor funkcie!

---
+++ Projekt/funkcie.c
@@ -0,0 +1,25 @@
+/***********************ial.c************************************/
+/* Soubor: ial.c - Obsahujicí funkce do předmětu IAL 			*/
+/* Předmět: Formalní jazyky a překladače (IFJ) 					*/
+/* Projekt: Implementace interpretu imperativního jazyka IFJ14  */
+/* Varianta zadání: b/1/I 										*/
+/* Datum: prosinec 2014											*/
+/* Kódování: UTF-8												*/
+/* Autoři:			Filip Kalous (xkalou03)						*/
+/****************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int length(s)
+{
+	
+}
+
+
+
+int main()
+{
+	
+
+}
