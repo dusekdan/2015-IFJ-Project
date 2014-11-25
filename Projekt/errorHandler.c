232ab3679ac9f4f5e32b4bf2f8d0788ffe106d0c
rsn4ke <rsn4ke@gmail.com>
2014-11-25 12:38:03 +0100

    errorhandler

---
+++ Projekt/errorHandler.c
@@ -0,0 +1,13 @@
+#include "errorHandler.h"
+
+int errorHandler (int errorType)
+{
+    /*switch (errorType)
+    {
+        case 1:     break;
+        default:    printf("Unspecified error occured.\n");
+                    break;
+    }*/
+    printf("chyba %d\n",errorType );
+    exit(errorType);
+}
